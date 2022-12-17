5f1f4fcdaa1d274a4d5427253e084d7252143e97
Bryan Mancilla <70993543@certus.edu.pe>
2022-12-17 00:08:02 -0500

    configuraciones adicionales

--- src/main/resources/static/Js/Script.js
+++ src/main/resources/static/Js/Script.js
@@ -1,3 +1,11 @@
-function limpiarcampos() {
-    window.location='/mg/listarproducto';
+let btn_clean = document.querySelector(".btn-clean");
+
+btn_clean.addEventListener("click",()=>{
+    limpiarcampos('/mg/listarproducto')
+})
+
+function limpiarcampos(ruta) {
+    window.location= ruta;
+    return ruta;
 }
+
